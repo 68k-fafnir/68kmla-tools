0927be1a3f37d225921ab932b52c6e69d0756431
Fafnir <newowlbiss2@gmail.com>
2025-03-04 18:15:51 +0000

    fix

--- doomsday.py
+++ doomsday.py
@@ -4,7 +4,8 @@ FORUM_URL = '68kmla.org'
 START_POST = 1
 BIG_NUMBER = 100000  #this should be larger than the total number of posts
 
-def doomsday_prep(url, start_post_id, latest_post_id): 
+def doomsday_prep(url, start_post_id, latest_post_id):
+    fail_404s = 0
     read_logins = 0
     read_fails = 0
     read_goods = 0
@@ -15,22 +16,23 @@ def doomsday_prep(url, start_post_id, latest_post_id):
         thread_url = base_path + str(threadid)
 
         r = requests.get(thread_url) # TODO: auth
-        print(r.text)
         # the actual thread title. use this to determine when we've read every page. also, don't parse html like this
         human_title = r.text.split('<h1 class="p-title-value">')[-1].split('</h1>')[0]
         # tab title can show 404 message
         tab_title = r.text.split('<title>')[1].split('</title>')[0]
 
-        if human_title.lower == "oops! we ran into some problems. | 68kmla" or tab_title.lower == '404 not found':
+        if '404' in tab_title:
+            fail_404s += 1
+            errors_in_a_row += 1
+            print("404: " + str(threadid))
+            if errors_in_a_row >= 10:
+                return("10 failed reads in a row. Either we've read every thread, the site is down, or I broke the code (hint: it's probably 3)\n\n"+str(read_goods+read_logins+read_fails-errors_in_a_row)+" total threads\n" + str(read_goods)+" sucessful reads\n" + str(read_logins) + " threads that require login\n" + str(read_fails)+" failed reads")
+        elif human_title.lower == "oops! we ran into some problems. | 68kmla":
             read_fails += 1
             errors_in_a_row += 1
             print("fail to read: " + str(threadid))
             if errors_in_a_row >= 10:
-                return("10 failed reads in a row. Either we've read every thread, or the site is down.\n\n"+str(read_goods+read_logins+read_fails-errors_in_a_row)+" total threads\n" + str(read_goods)+" sucessful reads\n" + str(read_logins) + " threads that require login\n" + str(read_fails)+" failed reads")
-        elif 'log in' in human_title.lower():
-            read_logins += 1
-            errors_in_a_row = 0
-            print("login protected: " + str(threadid))
+                return("10 failed reads in a row. Either we've read every thread, or the site is down.\n\n"+str(read_goods+read_logins+read_fails-errors_in_a_row)+" total threads\n" + str(read_goods)+" sucessful reads\n" + str(read_fails)+" failed reads\nsome threads that require login might exist i don't track them here\n")
         else:
             read_goods += 1
             errors_in_a_row = 0
